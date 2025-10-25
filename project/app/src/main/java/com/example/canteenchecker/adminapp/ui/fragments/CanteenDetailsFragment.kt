a85866be2e92cd9f4aba1454e797ba939530b438
Markus Ortner <s2310307025@fhooe.at>
2025-10-25 19:28:37 +0200

    added layout for detail overview

--- project/app/src/main/java/com/example/canteenchecker/adminapp/ui/fragments/CanteenDetailsFragment.kt
+++ project/app/src/main/java/com/example/canteenchecker/adminapp/ui/fragments/CanteenDetailsFragment.kt
@@ -1,6 +1,12 @@
 package com.example.canteenchecker.adminapp.ui.fragments
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.canteenchecker.adminapp.R
 
-class CanteenDetailsFragment : Fragment(R.layout.fragment_canteen_details)
+class CanteenDetailsFragment : Fragment(R.layout.fragment_canteen_details){
+    private var editingCanteenData = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
